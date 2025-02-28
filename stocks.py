0c5cf90d5c1725dface6cf2c3add8e65f6950985
esraa1000 <esraaaboelkhair8@gmail.com>
2025-02-28 10:18:15 +0200

    update stock model

--- stocks.py
+++ stocks.py
@@ -47,18 +47,16 @@ def build_lstm_model(input_shape):
     """
     Create an LSTM model for stock price prediction.
     """
+    # Build the LSTM model
     model = Sequential()
-    model.add(LSTM(units=50, activation='relu', return_sequences=True, input_shape=input_shape))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units=60, activation='relu', return_sequences=True))
-    model.add(Dropout(0.3))
-    model.add(LSTM(units=80, activation='relu', return_sequences=True))
-    model.add(Dropout(0.4))
-    model.add(LSTM(units=120, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(units=1))
+    model.add(LSTM(128, return_sequences=True, input_shape= (x_train.shape[1], 1)))
+    model.add(LSTM(64, return_sequences=False))
+    model.add(Dense(25))
+    model.add(Dense(1))
 
+    # Compile the model
     model.compile(optimizer='adam', loss='mean_squared_error')
+
     return model
 
 import numpy as np
